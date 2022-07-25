70a6067cb12012137139896dc1dc94293b6ddc1d
MRERROR <62744431+MRERRORPM@users.noreply.github.com>
2022-07-25 22:48:58 +0530

    Update __init__.py

--- plugins/fun/circle/__init__.py
+++ plugins/fun/circle/__init__.py
@@ -6,9 +6,4 @@
 #
 # All rights reserved.
 
-""" create carbon """
-
-import os
-
-GOOGLE_CHROME_DRIVER = os.environ.get("GOOGLE_CHROME_DRIVER")
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
+""" create circle """
